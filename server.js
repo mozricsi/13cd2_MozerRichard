0d289c0d2e0bdba8ebaab3dee1184dce13f77518
mozricsi <mozric721@hengersor.hu>
2025-02-17 14:32:25 +0100

    select használata

--- server.js
+++ server.js
@@ -2,30 +2,45 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
-const bodyparser= require("body-parser");
+const bodyparser = require("body-parser");
+ 
 app.use(cors());
 app.use(bodyparser.json());
-
+ 
 const db = mysql.createConnection(
     {
-        user:"root",
-        host:"localhost",
-        port:"3307",
-        password:"",
-        database:"teliolimpia"
-
+        user: "root",
+        host: "localhost",
+        port: "3307",
+        password: "",
+        database: "teliolimpia"
+ 
+    })
+//A szerver futásásak ellenőrzése
+ 
+app.get("/", (req, res) => {
+    res.send("A szerver működik!")
+})
+app.get("/v", (req, res) => {
+    const sql = "SELECT * FROM versenyzok";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
     }
-
-)
-app.get("/",(req,res) => {
-    res.send("A szerver működik")
+    )
 }
-
+)
+app.get("/v6/:id",(req, res) => {
+    const sql = "SELECT * FROM `versenyzok` WHERE ID= 6";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+    )
+}
 )
 
-// a  szerver futádának ellenőrzése
-
-
-app.listen(3000, ()=>{
-    console.log('A szerver a 3000 porton fut!')
+ 
+app.listen(3000, () => {
+    console.log('A szerver a 3000-es porton fut!')
 })
